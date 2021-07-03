beac875e42dd8f45a7b2389b522f967ab4dbcff3
LPlamondon <LPlamondon@users.noreply.github.com>
2021-07-03 13:52:31 -0700

    Updated header.jsx to have a return statement

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,25 +2,27 @@ import React, { Component } from 'react';
 
 class Header extends Component {
   render(){
-    <React.Fragment>
-      <div class="logo">
-        <img class="rounded-circle mx-auto" alt="logo" src="../images/Lily_Logo.png" />
-      </div>
-      <div class="content">
-        <div class="inner">
-        <h1>Lily Plamondon</h1>
-        <p>A website created to showcase my porfolio using React and basing the look off a template from <a href="https://html5up.net">HTML5 UP</a>, but made to be my own.</p>
+    return (
+      <React.Fragment>
+        <div class="logo">
+          <img class="rounded-circle mx-auto" alt="logo" src="../images/Lily_Logo.png" />
         </div>
-      </div>
-      <nav>
-      <ul>
-        <li><a href="#intro">Intro</a></li>
-        <li><a href="#work">Work</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#contact">Contact</a></li>
-      </ul>
-      </nav>
-    </React.Fragment>
+        <div class="content">
+          <div class="inner">
+            <h1>Lily Plamondon</h1>
+            <p>A website created to showcase my porfolio using React and basing the look off a template from <a href="https://html5up.net">HTML5 UP</a>, but made to be my own.</p>
+          </div>
+        </div>
+        <nav>
+          <ul>
+            <li><a href="#intro">Intro</a></li>
+            <li><a href="#work">Work</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </nav>
+      </React.Fragment>
+  );
   }
 }
 
