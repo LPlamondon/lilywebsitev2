4fa0652f396ff60faca96915b115337a234b3b18
LPlamondon <LPlamondon@users.noreply.github.com>
2021-07-03 14:28:06 -0700

    Include main.scss in index.js and added stuff to main.scss (Will change later)

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
+import './sass/main.scss'
 
 //Component imports
 import Header from './components/header';
