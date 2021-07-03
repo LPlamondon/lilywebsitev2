c51cc6c80096afb7c2ecfaea6d5bd87480fed6ab
LPlamondon <LPlamondon@users.noreply.github.com>
2021-07-02 19:01:33 -0700

    Created footer.jsx

---
+++ src/components/footer.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render(){
+    <React.Fragment>
+      <p>&copy; Lily.Plamondon. Design: <a href="https://html5up.net">HTML5 UP</a> and me</p>
+    </React.Fragment>
+  }
+}
+
+export default Footer;
