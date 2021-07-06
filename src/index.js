49ce0beb919e66eea0e0874da5c0bf3d4e4273ce
LPlamondon <LPlamondon@users.noreply.github.com>
2021-07-05 17:29:26 -0700

    Updated index.js to use new scss files and components

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
-import './sass/main.scss'
+import './sass/header.scss'
+import './sass/body.scss'
 
 //Component imports
 import Header from './components/header';
 import Footer from './components/footer';
+import Index from './components/index';
 
 ReactDOM.render(<Header />, document.getElementById("headroot"));
+ReactDOM.render (<Index />, document.getElementById("root"));
 ReactDOM.render(<Footer />, document.getElementById("footroot"));
