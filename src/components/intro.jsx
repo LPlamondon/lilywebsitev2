a14e71d159bfe513ae05da295091fc272cb235d4
LPlamondon <LPlamondon@users.noreply.github.com>
2021-07-07 15:02:49 -0700

    Created intro component and based it off index.jsx

---
+++ src/components/intro.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Intro extends Component {
+  render(){
+    return (
+      <React.Fragment>
+        <div class="pages">
+          <div class="pageContent">
+            <h1>Lily Plamondon</h1>
+            <p>A website created to showcase my porfolio and experience.</p>
+            <p>This website was created using React!</p>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Intro;
