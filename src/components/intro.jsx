937f2e3947c66375e7cd67e496c2c6b3bdd51ab8
LPlamondon <LPlamondon@users.noreply.github.com>
2021-07-07 15:08:45 -0700

    Updated intro.jsx with temporary content

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -6,9 +6,8 @@ class Intro extends Component {
       <React.Fragment>
         <div class="pages">
           <div class="pageContent">
-            <h1>Lily Plamondon</h1>
-            <p>A website created to showcase my porfolio and experience.</p>
-            <p>This website was created using React!</p>
+            <h1>Intro</h1>
+            <p>Stuff goes here later on</p>
           </div>
         </div>
       </React.Fragment>
