972fb28b031fe21362e7cf6449c6bdcad8663cb9
LPlamondon <LPlamondon@users.noreply.github.com>
2021-07-08 16:26:24 -0700

    Wrote up Intro. Will find image later

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -6,8 +6,34 @@ class Intro extends Component {
       <React.Fragment>
         <div class="pages">
           <div class="pageContent">
-            <h1>Intro</h1>
-            <p>Stuff goes here later on</p>
+            <h2>Intro</h2>
+            <img src = ""/>
+            <h4>Who am I?</h4>
+            <p>
+              My name is Lily Plamondon. I went to Simon Fraser University, and graduated back in 2019. I currently live in the Greater Vancouver area.
+              I speak both English and Japanese, the first one being my native langauge while the latter I learned in university.
+              My hobbies include reading, writing short stories, playing video games, and watching anime.
+            </p>
+            <p>
+              One of my favourite games is Final Fantasy 14, where I play as a Miqo'te. My main job in it is scholar, where I function as the party healer. This fits in well with
+              my personality, as I prefer to quietly support others instead of taking the main focus. For books, I've mainly been reading manga lately, although if I find a good
+              novel I won't hesitate to pick it up and read it, often powering through in one day.
+            </p>
+            <h4>What skills do I have?</h4>
+            <p>
+              I know HTML5, CSS, and Javascript. For web development frameworks, I also have experience with SASS, React, Ruby on Rails, Bootstrap, and Node.js.
+              Besides web development, I am familiar with Python, C, C++, and Java. I also have some experience with Unity and C#.
+
+              Besides programming languages and frameworks, I've also learned how to work in a team, communication, UI design, and mediation skills.
+            </p>
+            <h4>Anything else interesting about you?</h4>
+            <p>
+            I have a twin brother named Thomas, who's only about 2-3 minutes older than me. Growing up with a sibling I could always depend on and get along with has been
+            an amazing experience, and I love him dearly.
+
+            I also have dual citizenship, Canadian and Swiss. This is due to my mom coming from Switzerland, giving me Swiss citzenship by blood. I can speak a minor amount of
+            German, although my Japanese is far better. 
+            </p>
           </div>
         </div>
       </React.Fragment>
