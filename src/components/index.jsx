544f0b31a8753d4a04fe0b6821626e1c9f86e5b7
LPlamondon <LPlamondon@users.noreply.github.com>
2021-07-08 16:01:29 -0700

    Added rounded class (bootstrap) to content div in index.jsx

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -4,7 +4,7 @@ class Index extends Component {
   render(){
     return (
       <React.Fragment>
-        <div class="content">
+        <div class="content rounded">
           <div class="inner">
             <h1>Lily Plamondon</h1>
             <p>A website created to showcase my porfolio and experience.</p>
