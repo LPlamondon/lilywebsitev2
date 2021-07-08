6820c1de6aa3c69dd7b55b288d067297b393e12e
LPlamondon <LPlamondon@users.noreply.github.com>
2021-07-08 16:55:35 -0700

    Updated intro.jsx

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -19,22 +19,26 @@ class Intro extends Component {
               my personality, as I prefer to quietly support others instead of taking the main focus. For books, I've mainly been reading manga lately, although if I find a good
               novel I won't hesitate to pick it up and read it, often powering through in one day.
             </p>
+            <p>
+              I also play a Dark Heresey 2 TTRPG homebrew with a group of online friends, where I have experience both as a player and as a gamemaster. This has been a very valuable
+              experience for me to improve my creativity, as well as improving my communication skills.
+            </p>
             <h4>What skills do I have?</h4>
             <p>
               I know HTML5, CSS, and Javascript. For web development frameworks, I also have experience with SASS, React, Ruby on Rails, Bootstrap, and Node.js.
               Besides web development, I am familiar with Python, C, C++, and Java. I also have some experience with Unity and C#.
             </p>
             <p>
-            Besides programming languages and frameworks, I've also learned how to work in a team, communication, UI design, and mediation skills.
+              Besides programming languages and frameworks, I've also learned how to work in a team, communication, UI design, and mediation skills.
             </p>
             <h4>Anything else interesting about you?</h4>
             <p>
-            I have a twin brother named Thomas, who's only about 2-3 minutes older than me. Growing up with a sibling I could always depend on and get along with has been
-            an amazing experience, and I love him dearly.
+              I have a twin brother named Thomas, who's only about 2-3 minutes older than me. Growing up with a sibling I could always depend on and get along with has been
+              an amazing experience, and I love him dearly.
             </p>
             <p>
-            I also have dual citizenship, Canadian and Swiss. This is due to my mom coming from Switzerland, giving me Swiss citzenship by blood. I can speak a minor amount of
-            German, although my Japanese is far better.
+              I also have dual citizenship, Canadian and Swiss. This is due to my mom coming from Switzerland, giving me Swiss citzenship by blood. I can speak a minor amount of
+              German, although my Japanese is far better.
             </p>
           </div>
         </div>
