a73dba0275b741f8d1cb76a1b755bed007aea4f2
LPlamondon <LPlamondon@users.noreply.github.com>
2021-07-08 17:27:29 -0700

    Created about.jsx

---
+++ src/components/about.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+  render(){
+    return (
+      <React.Fragment>
+        <div class="pages rounded">
+          <div class="pageContent">
+            <h2>Intro</h2>
+            <img src = "" />
+            <p>
+              This website was built using React and Bootstrap, and hosted on AWS. 
+            </p>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default About;
