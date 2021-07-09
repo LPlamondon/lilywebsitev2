ae857e9c84f8b61671b33723fef967b5095736cb
LPlamondon <LPlamondon@users.noreply.github.com>
2021-07-08 17:39:06 -0700

    Created contact.jsx

---
+++ src/components/contact.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+  render(){
+    return (
+      <React.Fragment>
+        <div class="pages rounded">
+          <div class="pageContent">
+            <h2>Contact</h2>
+            <img src = "" />
+            <p>
+              Please email me at <a href="mailto:lcplamond@gmail.com">lcplamond@gmail.com</a> if you would like to contact me.
+            </p>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Contact;
