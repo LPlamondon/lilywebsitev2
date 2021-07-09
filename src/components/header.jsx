9e380100bc541cbea909ef8bd5fefb61b5b21679
LPlamondon <LPlamondon@users.noreply.github.com>
2021-07-08 19:27:19 -0700

    Added imports for routing, and switched a tags to link tags

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,4 +1,10 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import logo from '../images/Lily_Logo.png';
 
 class Header extends Component {
@@ -8,14 +14,17 @@ class Header extends Component {
         <div class="logo">
           <img class="rounded-circle mx-auto" id = "lily-logo" alt="logo" src={logo} />
         </div>
+        <Router>
         <nav>
           <ul>
-            <li><a href="#intro">Intro</a></li>
-            <li><a href="#work">Work</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/intro'>Intro</Link></li>
+            <li><Link to='/work'>Work</Link></li>
+            <li><Link to='/about'>About</Link></li>
+            <li><Link to='/contact'>Contact</Link></li>
           </ul>
         </nav>
+        </Router>
       </header>
     );
   }
