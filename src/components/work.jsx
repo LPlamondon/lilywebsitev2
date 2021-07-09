3f5124c284be91e289e0020b289e2ad6769ba2d9
LPlamondon <LPlamondon@users.noreply.github.com>
2021-07-08 17:15:46 -0700

    Updated work.jsx to use h6 instead of h4

--- src/components/work.jsx
+++ src/components/work.jsx
@@ -9,18 +9,18 @@ class Work extends Component {
             <h2>Work</h2>
             <img src = "" />
             <p>My github can be found <a href="https://github.com/LPlamondon">here</a>, while you can download my resume [HERE]</p>
-            <h4>2021</h4>
+            <h6>2021</h6>
               <ul>
                 <li><a href="https://www.vancouverdatajam.ca/">Vancouver Datjam - Currently working as a volunteer web developer</a></li>
                 <li><a href="https://github.com/tplamondon/eilacogs">Eilacogs as a co-helper</a></li>
                 <li><a href="https://github.com/SFUAnime/Ren">Ren discord bot for SFU anime</a></li>
               </ul>
-            <h4>2020</h4>
+            <h6>2020</h6>
             <ul>
-              <li><p>Private tutor for Elementary and High-school children</p></li>
+              <li>Private tutor for Elementary and High-school children</li>
               <li><a href="https://www.vancouverdatajam.ca/">2020 Vancouver Datajam as a volunteer web developer</a></li>
             </ul>
-            <h4>2019</h4>
+            <h6>2019</h6>
               <ul>
                 <li><a href="https://github.com/nyhchang/NicoBot">Nico discord bot</a></li>
               </ul>
